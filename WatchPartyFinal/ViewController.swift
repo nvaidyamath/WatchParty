ded83a92c53ccc99bb77beb4268954d672a4bd58
nvaidyamath <nuv200@nyu.edu>
2020-04-26 14:43:39 -0700

    added comment to see if git works

--- WatchPartyFinal/ViewController.swift
+++ WatchPartyFinal/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //testing to see if git pushes changes
         // Do any additional setup after loading the view.
     }
 
