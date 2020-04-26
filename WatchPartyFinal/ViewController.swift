47125b14e5fddf88df47113f1d322ece3ac3bf9f
nvaidyamath <nuv200@nyu.edu>
2020-04-26 14:15:57 -0700

    Initial Commit

---
+++ WatchPartyFinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchPartyFinal
+//
+//  Created by Nikhil Vaidyamath on 4/26/20.
+//  Copyright © 2020 Nikhil Vaidyamath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
