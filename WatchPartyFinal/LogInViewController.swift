be4af7c357b0a4a5490c15ea7b497ca4ffe1df4c
Antoine Assaf <aca432@nyu.edu>
2020-04-27 17:40:42 -0400

    Added Outlets

--- WatchPartyFinal/LogInViewController.swift
+++ WatchPartyFinal/LogInViewController.swift
@@ -9,22 +9,18 @@
 import UIKit
 
 class LogInViewController: UIViewController {
-
+    @IBOutlet var emailField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    @IBOutlet var loginButton: UIButton!
+    @IBOutlet var errorDisplay: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        errorDisplay.alpha = 0;
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func loginPressed(_ sender: Any) {
     }
-    */
-
+    
 }
--- WatchPartyFinal/SignUpViewController.swift
+++ WatchPartyFinal/SignUpViewController.swift
@@ -9,22 +9,20 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-
+    @IBOutlet var firstNameField: UITextField!
+    @IBOutlet var lastNameField: UITextField!
+    @IBOutlet var emailField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    @IBOutlet var signUpButton: UIButton!
+    @IBOutlet var errorDisplay: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        errorDisplay.alpha = 0;
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func SignUpPressed(_ sender: Any) {
     }
-    */
-
+    
 }
