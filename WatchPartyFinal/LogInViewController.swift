5712279ce5c1db59562514141bad75f6afd84b5b
Antoine Assaf <aca432@nyu.edu>
2020-04-27 14:04:54 -0400

    Added main view controllers for authentication, use homescreen for anything beyond that

---
+++ WatchPartyFinal/LogInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LogInViewController.swift
+//  WatchPartyFinal
+//
+//  Created by Antoine Assaf on 4/27/20.
+//  Copyright © 2020 Nikhil Vaidyamath. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
