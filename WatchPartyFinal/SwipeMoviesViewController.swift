16405120ee7641984b41f8a2f9e41609d19a6c9e
nvaidyamath <nuv200@nyu.edu>
2020-04-28 17:02:44 -0700

    created gesture recognizer and detected if user is interested/not interested

---
+++ WatchPartyFinal/SwipeMoviesViewController.swift
@@ -0,0 +1,68 @@
+//
+//  SwipeMoviesViewController.swift
+//  WatchPartyFinal
+//
+//  Created by Nikhil Vaidyamath on 4/28/20.
+//  Copyright © 2020 Nikhil Vaidyamath. All rights reserved.
+//
+
+import UIKit
+
+class SwipeMoviesViewController: UIViewController {
+
+    @IBOutlet var movieObjectView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(wasDragged(gestureRecognizer:)))
+             movieObjectView.addGestureRecognizer(gesture)
+             // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.
+    }
+    @objc func wasDragged(gestureRecognizer: UIPanGestureRecognizer) {
+               print("dragged")
+              let labelPoint = gestureRecognizer.translation(in: view)
+              movieObjectView.center = CGPoint(x: view.bounds.width / 2 + labelPoint.x, y: view.bounds.height / 2 + labelPoint.y)
+              
+              let xFromCenter = view.bounds.width / 2 - movieObjectView.center.x
+
+              var rotation = CGAffineTransform(rotationAngle: xFromCenter / 200)
+
+              let scale = min(100 / abs(xFromCenter), 1)
+
+              var scaledAndRotated = rotation.scaledBy(x: scale, y: scale)
+
+              movieObjectView.transform = scaledAndRotated
+
+              if gestureRecognizer.state == .ended {
+
+                  if movieObjectView.center.x < (view.bounds.width / 2 - 100) {
+                      print("Not Interested")
+                      
+                      
+                  }
+                  if movieObjectView.center.x > (view.bounds.width / 2 + 100) {
+                      print("Interested")
+                  }
+
+                  rotation = CGAffineTransform(rotationAngle: 0)
+
+                  scaledAndRotated = rotation.scaledBy(x: 1, y: 1)
+
+                  movieObjectView.transform = scaledAndRotated
+
+                  movieObjectView.center = CGPoint(x: view.bounds.width / 2, y: view.bounds.height / 2)
+              }
+          }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- WatchPartyFinal/ViewController.swift
+++ WatchPartyFinal/ViewController.swift
@@ -25,38 +25,38 @@ class ViewController: UIViewController {
     
     override func viewWillLayoutSubviews() {
         //Initial video background
-        playVideo()
+        //playVideo()
     }
     
-    func playVideo(){
-        // Get the path to the resource in the bundle
-        let bundlePath = Bundle.main.path(forResource: "logInVideo", ofType: "mp4")
-        
-        guard bundlePath != nil else {
-            return
-        }
-        
-        // Create a URL from it
-        let url = URL(fileURLWithPath: bundlePath!)
-        
-        // Create the video player item
-        let item = AVPlayerItem(url: url)
-        
-        // Create the player
-        videoPlayer = AVPlayer(playerItem: item)
-        
-        // Create the layer
-        videoPlayerLayer = AVPlayerLayer(player: videoPlayer)
-        
-        // Adjust the size and frame
-        videoPlayerLayer?.frame = CGRect(x: -self.view.frame.size.width*1.5, y: 0, width: self.view.frame.size.width*4, height: self.view.frame.size.height)
-        
-        view.layer.insertSublayer(videoPlayerLayer!, at: 0)
-        
-        // Add it to the view and play it
-        videoPlayer?.playImmediately(atRate: 0.3)
-        
-    }
+//    func playVideo(){
+//        // Get the path to the resource in the bundle
+//        let bundlePath = Bundle.main.path(forResource: "logInVideo", ofType: "mp4")
+//        
+//        guard bundlePath != nil else {
+//            return
+//        }
+//        
+//        // Create a URL from it
+//        let url = URL(fileURLWithPath: bundlePath!)
+//        
+//        // Create the video player item
+//        let item = AVPlayerItem(url: url)
+//        
+//        // Create the player
+//        videoPlayer = AVPlayer(playerItem: item)
+//        
+//        // Create the layer
+//        videoPlayerLayer = AVPlayerLayer(player: videoPlayer)
+//        
+//        // Adjust the size and frame
+//        videoPlayerLayer?.frame = CGRect(x: -self.view.frame.size.width*1.5, y: 0, width: self.view.frame.size.width*4, height: self.view.frame.size.height)
+//        
+//        view.layer.insertSublayer(videoPlayerLayer!, at: 0)
+//        
+//        // Add it to the view and play it
+//        videoPlayer?.playImmediately(atRate: 0.3)
+//        
+//    }
     
     func initUI(){
         UIUtilities.styleFilledButton(signUpButton)
